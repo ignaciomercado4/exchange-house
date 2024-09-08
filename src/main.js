d02bf7a669c818ea3df0a9c73e9da7152a548f1b
Ignacio Mercado <igmercado46@gmail.com>
2024-09-08 00:54:19 -0400

    adds API fetch

--- src/main.js
+++ src/main.js
@@ -1,3 +1,22 @@
-const BASE_API_URL = "https://api.exchangerate-api.com/v4/latest/";
+const BASE_API_URL = "https://v6.exchangerate-api.com/v6/d8cb21195343f64d6681c718/latest/";
+const $initialCurrency = document.querySelector('#initial-currency');
+const $finalCurrency = document.querySelector('#final-currency');
+const $initialAmount = document.querySelector('#initial-amount');
+const $finalAmount = document.querySelector('#final-amount');
+const $calculateButton = document.querySelector('#calculate');
+let finalAmount = 0;
 
-console.log(BASE_API_URL)
+
+$calculateButton.onclick = function () {
+    if ($initialAmount.value && $initialCurrency.value) {
+        fetch(BASE_API_URL + $initialCurrency.value)
+            .then(response => response.json())
+            .then(responseJSON => a(responseJSON))
+            .catch(error => console.log(error))
+
+    }
+}
+
+function a(res) {
+    console.log(res)
+}
