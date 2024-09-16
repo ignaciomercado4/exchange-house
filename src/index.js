d304479b12dcfac581817d292d65991e4b8fd691
Ignacio Mercado <igmercado46@gmail.com>
2024-09-16 00:50:23 -0400

    adds mini error dislpaying function

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import {
     $finalCurrency,
     $initialAmount,
     $calculateButton,
-    displayFinalAmount
+    displayFinalAmount,
+    displayError,
+    hideErrors
 } from './ui.js';
 
 import {
@@ -13,12 +15,18 @@ import {
 const BASE_API_URL = "https://v6.exchangerate-api.com/v6/d8cb21195343f64d6681c718/latest/";
 
 function calculateFinalAmount() {
+
     if ($initialAmount.value && $initialCurrency.value) {
+
+        hideErrors();
+
         fetch(BASE_API_URL + $initialCurrency.value)
             .then(response => response.json())
             .then(responseJSON => convertInitialAmount(responseJSON))
             .then(finalAmount => displayFinalAmount(finalAmount, $finalCurrency.value))
-            .catch(error => console.log(error))
+            .catch(error => displayError(error))
+    } else {
+        displayError('Invalid/empty inputs.');
     }
 }
 
--- src/ui.js
+++ src/ui.js
@@ -3,8 +3,17 @@ export const $finalCurrency = document.querySelector('#final-currency');
 export const $initialAmount = document.querySelector('#initial-amount');
 export const $finalAmount = document.querySelector('#final-amount');
 export const $calculateButton = document.querySelector('#calculate');
+export const $errorContainer = document.querySelector('#error');
 
 export function displayFinalAmount(finalAmount, finalCurrency) {
 
     $finalAmount.value = finalAmount.toFixed(2) + " " + finalCurrency;
+}
+
+export function displayError(errorMessage) {
+    $errorContainer.textContent = errorMessage;
+    $errorContainer.classList.replace('hidden', 'block');
+}
+export function hideErrors() {
+    $errorContainer.classList.replace('block', 'hidden');
 }
